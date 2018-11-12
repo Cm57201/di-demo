704df664783f8b2df320cc9c71f842779763019e
cmahajan <cmahajan@allegisgroup.com>
2018-11-12 18:04:27 -0500

    no message

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -20,5 +20,6 @@ public class DiDemoApplication {
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		
 	}
 }
